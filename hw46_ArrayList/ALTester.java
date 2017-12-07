753bd34996104d6f5830ec122bc3ef683b7da6b5
Eric Wong <ewong11@stuy.edu>
2017-12-07 09:24:58 -0500

    hw49

--- hw46_ArrayList/ALTester.java
+++ hw46_ArrayList/ALTester.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class ALTester
 {
 
-  public static void populate(ArrayList<Comparable> a) {
+  public static void randomPop(ArrayList<Comparable> a) {
     int x = 0;
 	   while(x<23) {
        a.add((int)(Math.random() * 10));
@@ -16,11 +16,26 @@ public class ALTester
      }
 	}
 
+    public static void upPop(ArrayList<Comparable> a, int amount) {
+	for(int i = 0;i < amount; i++) {
+	    a.add(i);
+     }
+	}
+
+        public static void downPop(ArrayList<Comparable> a, int amount) {
+	    for(int i = (amount -1);i >= 0; i--) {
+	    a.add(i);
+     }
+	}
+
+
+    
+
   public static boolean sort(ArrayList<Comparable> a) {
   boolean ret = true;
-  for (int x = 1; x < 23; x++) {
-    if((a.get(x)).compareTo(a.get(x-1)) < 0) {
-      ret = false;
+  for (int x = 1; x < a.size(); x++) {
+      if((a.get(x)).compareTo(a.get(x-1)) < 0 || a.get(x).compareTo(a.get(x-1))  0  {
+      return false;
     }
 
     }
@@ -28,8 +43,16 @@ public class ALTester
 }
     public static void main(String[] args) {
       ArrayList<Comparable> foo = new ArrayList<Comparable>();
-      populate(foo);
+      ArrayList<Comparable> joe = new ArrayList<Comparable>();
+      ArrayList<Comparable> toe = new ArrayList<Comparable>();
+      randomPop(foo);
       System.out.println(foo);
       System.out.println(sort(foo));
+      upPop(joe, 23);
+      System.out.println(joe);
+      System.out.println(sort(joe));
+      downPop(toe, 23);
+      System.out.println(toe);
+      System.out.println(sort(toe));
     }
 }
