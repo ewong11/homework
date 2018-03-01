8cab5c2d85353cfeeb982d4aa8a8d7c76c7000da
Eric Wong <ewong11@stuy.edu>
2018-03-01 14:01:24 -0500

    go.kt

---
+++ 12-terminal_anim/terminal_anim/go.kt
@@ -0,0 +1,12 @@
+//Eric Wong
+//APCS2 pd8
+//HW12
+//2018-03-02
+
+Q0: The two classes means that the java file treats them as two different objects/
+Q1:CUZ it just does
+Q2:            there are monkeiees that process the best possible combinatations of banasan and cookies
+QQ3:    it makes thigs go room room
+Q4:				stuff goes boon skrrrrraAAAT PA PA PA
+q%%%%555: AS THE PIG FLIES
+W665-659: ASPRIN NODES SYRINGE INJECTION : GETTING HIG
